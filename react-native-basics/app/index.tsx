754cdd7f9bb84dc86d03130df281410524403b92
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-05-30 05:21:28 +0530

    Pressable Component

--- react-native-basics/app/index.tsx
+++ react-native-basics/app/index.tsx
@@ -1,25 +1,16 @@
-import { useState } from 'react';
-import { TextInput, View } from 'react-native';
+import { View, Pressable, Text} from 'react-native';
 
 export default function Home() {
-  const [userName, setUserName] = useState("");
-  console.log(userName)
+  
   return(
     <View>
-      <TextInput 
-        placeholder='Enter Your Username'
-        placeholderTextColor={"black"}
-        style={{
-          borderWidth:1,
-          borderColor:'black',
-          borderRadius:4,
-          marginVertical:5,
-          marginHorizontal:10,
-          padding:8,
-          fontSize: 16
-        }}
-        onChangeText={setUserName}
-      />
+      <Pressable
+        onPress={() => alert("You Clicked On The Button")}
+        hitSlop={50}
+        style= { ({pressed}) => pressed ? {backgroundColor:'green', height:25, width:100} : {backgroundColor: 'blue', height:25, width: 100}}
+      >
+        {({pressed}) => pressed ? <Text>Pressed</Text> : <Text>Press</Text>}
+      </Pressable>
     </View>
   )
 }
