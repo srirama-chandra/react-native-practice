a1f3cc9971c50d6ca80cc569a70bec53f2b4aff8
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-05-30 07:04:12 +0530

    switch

--- react-native-basics/app/index.tsx
+++ react-native-basics/app/index.tsx
@@ -1,28 +1,19 @@
-import { View, Pressable, Text, ScrollView } from "react-native";
+
+import { useState } from "react";
+import { View, Pressable, Text, ScrollView, Button, Switch } from "react-native";
 
 export default function Home() {
-  const arr = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2,
-    3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4,
-    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <ScrollView>
-      {arr.map((item, index) => {
-        return <View key={index} style={{
-          backgroundColor: "white",
-          padding: 16,
-          borderRadius: 10,
-          marginBottom: 10,
-          shadowColor: '#000',
-          shadowOpacity: 0.5,
-          shadowRadius:4,
-          elevation:2
-        }}>
-          <Text>{item}</Text>
-        </View>
-      })}
+    <ScrollView style={{flex:1, padding:2, backgroundColor:'black'}}>
+      <Switch
+        value={darkMode}
+        onValueChange={setDarkMode}
+        thumbColor={'white'}
+        trackColor={{false:'#7E8274', true:'green'}}
+      />
     </ScrollView>
   );
 }
