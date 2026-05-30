66d6c7894c5580cd844e22f82444ec77740de3e3
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-05-30 08:27:32 +0530

    KeyboardAvoidingView & SafeAreaView

--- react-native-basics/app/index.tsx
+++ react-native-basics/app/index.tsx
@@ -1,48 +1,17 @@
 
-import { useState } from "react";
-import { View, Pressable, Text, ScrollView, Button, Switch, FlatList } from "react-native";
+import { View, Button, KeyboardAvoidingView, TextInput, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
-
-  const users = [
-  {
-    id: 1,
-    name: "Rahul Sharma",
-    email: "rahul@example.com",
-    age: 24,
-  },
-  {
-    id: 2,
-    name: "Priya Reddy",
-    email: "priya@example.com",
-    age: 27,
-  },
-  {
-    id: 3,
-    name: "Arjun Kumar",
-    email: "arjun@example.com",
-    age: 22,
-  },
-  {
-    id: 4,
-    name: "Sneha Patel",
-    email: "sneha@example.com",
-    age: 29,
-  },
-  {
-    id: 5,
-    name: "Vikram Singh",
-    email: "vikram@example.com",
-    age: 31,
-  },
-];
-
-  return(
-    <FlatList
-      data={users}
-      keyExtractor={ (item) => item.id.toString() }
-      renderItem={ ({item}) => <Text>{item.name}</Text>}
-      ItemSeparatorComponent={() => <View style={{height:1, backgroundColor:'black'}}/>}
-    />
+  return (
+    <SafeAreaView style={{flex:1}}>
+    <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS==="ios"? "padding": "height"} keyboardVerticalOffset={100}>
+      <View style={{flex:1, justifyContent:'flex-start'}}>
+        <TextInput style={{borderColor:'black', borderWidth:1}}/>
+        <TextInput style={{borderColor:'black', borderWidth:1}}/>
+        <Button title="Click" color={'green'}/>
+      </View>
+    </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
