9678a1188ba9ec8dafcd39e25461acc0a9cc12db
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-05-30 05:30:22 +0530

    ScrollView Component

--- react-native-basics/app/index.tsx
+++ react-native-basics/app/index.tsx
@@ -1,16 +1,21 @@
-import { View, Pressable, Text} from 'react-native';
+import { View, Pressable, Text, ScrollView} from 'react-native';
 
 export default function Home() {
   
   return(
-    <View>
-      <Pressable
-        onPress={() => alert("You Clicked On The Button")}
-        hitSlop={50}
-        style= { ({pressed}) => pressed ? {backgroundColor:'green', height:25, width:100} : {backgroundColor: 'blue', height:25, width: 100}}
-      >
-        {({pressed}) => pressed ? <Text>Pressed</Text> : <Text>Press</Text>}
-      </Pressable>
-    </View>
+    <ScrollView>
+      <Text style={{padding:16}}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quisquam numquam ducimus eaque excepturi, provident est! Ea doloremque architecto eveniet illo amet voluptas dolorem, ratione nisi earum. Recusandae, placeat possimus?
+        Quas pariatur recusandae tempore fugit nam, laborum numquam, minus dolorum qui, perferendis quos. Quisquam et ex incidunt odio aut tempora pariatur fuga labore reprehenderit assumenda, itaque id nobis impedit reiciendis.
+        Numquam, culpa dolorem? Doloribus quo commodi impedit repellat nobis ad repudiandae aspernatur debitis in quis labore, laborum incidunt ducimus nihil. Cum officia ut doloremque in odit sed fuga repellendus voluptate!
+        Saepe, nobis. Provident non officiis consequatur reiciendis quos, eum error nihil tempora quod fugiat velit neque natus similique eos repellat. Architecto nobis, illo praesentium nisi eius maxime obcaecati ipsa consequuntur!
+        Officia ratione saepe labore, unde pariatur minima. Quidem praesentium quaerat sequi iste corporis, iure repudiandae, voluptate officia tempora quasi eum natus itaque vel sed deserunt libero commodi perspiciatis numquam tenetur.
+        Totam nihil tempore fuga inventore cupiditate optio, eius dignissimos sit porro sed ipsam qui dolorum ullam, reprehenderit asperiores vitae nostrum, velit doloribus magni. Nemo vel, ut corporis laudantium neque praesentium.
+        Vitae, odit. Ea omnis nesciunt odit quibusdam, praesentium sed quae, porro ex nulla voluptate perferendis excepturi laboriosam quaerat inventore sint saepe, officiis sapiente id? Id aut necessitatibus voluptatem aliquid inventore!
+        Iusto placeat at, dolorem temporibus natus maxime officiis eius illo tempora nihil excepturi pariatur aperiam inventore accusamus quidem, deserunt vero. Impedit perspiciatis rerum expedita iusto neque voluptates, perferendis laborum dicta.
+        Facere, sunt. Odit iure atque libero inventore consequuntur blanditiis numquam, aperiam beatae id, repellat fugiat recusandae consequatur nobis? Quo quae amet rem cum eligendi eum, suscipit praesentium corrupti exercitationem temporibus?
+        Perspiciatis, architecto at libero maiores, laborum minus doloribus debitis iure alias dolor nisi. Molestias quasi quam facilis laborum incidunt nam deserunt, dolor provident beatae dignissimos accusamus, vero debitis, minima saepe.
+      </Text>
+    </ScrollView>
   )
 }
