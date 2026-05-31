473cfec899780ffb22a2a6728e9bb9a928483015
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-05-31 12:25:30 +0530

    screen-orientation

--- react-native-basics/app/index.tsx
+++ react-native-basics/app/index.tsx
@@ -1,17 +1,51 @@
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 const HomePage = () => {
-  const {height, width} = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
+
+  const onPotrait = async () => {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.PORTRAIT_UP,
+    );
+  };
+
+  const onLandscape = async () => {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.LANDSCAPE,
+    );
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text>HomePage</Text>
-      <Text>{width>768 ? "Landscape": "Potrait"}</Text>
+      <Text>{width > height ? "Landscape" : "Potrait"}</Text>
+      <View style={{ flexDirection: "row", gap: 5 }}>
+        <Pressable
+          onPress={onPotrait}
+          style={{ padding: 16, backgroundColor: "green" }}
+        >
+          <Text>Rotate Potrait</Text>
+        </Pressable>
+        <Pressable
+          onPress={onLandscape}
+          style={{ padding: 16, backgroundColor: "purple" }}
+        >
+          <Text>Rotate Landscape</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
