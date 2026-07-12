baea36c0a62a86910769aac8d1143eeb2777996d
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-07-12 13:47:10 +0530

    custom-tabs

--- react-native-expo-navigation/app/(tabs)/_layout.tsx
+++ react-native-expo-navigation/app/(tabs)/_layout.tsx
@@ -1,22 +1,38 @@
-import { NativeTabs, Label, Icon} from 'expo-router/unstable-native-tabs';
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { Tabs } from "expo-router";
+import { TouchableOpacity, View, Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+function MyTabBar({state, descriptors, navigation}: BottomTabBarProps) {
+    return <View style={{flexDirection:'row', padding:10, gap:4}}>
+        {state.routes.map((route,index) => {
+            const isFocused = state.index === index;
+            const { options } = descriptors[route.key];
+            const color = isFocused ? "red" : "black";
+
+
+            return <TouchableOpacity 
+                key={route.key}
+                onPress={ () => navigation.navigate(route.name)}
+                style={{flex:1, alignItems:'center',marginBottom:8, gap:2, backgroundColor:'cyan'}}
+            >
+                {options.tabBarIcon?.({
+                    focused: isFocused,
+                    color,
+                    size:24
+                })}
+                <Text style={{color: isFocused ? 'red' : 'black'}}>{options.title}</Text>
+                
+            </TouchableOpacity>
+
+        })}
+    </View>
+}
 
 export default function TabLayout() {
-    return (
-        <NativeTabs>
-            <NativeTabs.Trigger name='index'>
-                <Label>Home</Label>
-                <Icon sf="house.fill" drawable="custom_android_drawable" />
-            </NativeTabs.Trigger>
-
-            <NativeTabs.Trigger name='profile'>
-                <Label>Profile</Label>
-                <Icon sf="person" drawable="custom_android_drawable" />
-            </NativeTabs.Trigger>
-
-            <NativeTabs.Trigger name='settings'>
-                <Label>Settings</Label>
-                <Icon sf="airpodspro" drawable="custom_android_drawable" />
-            </NativeTabs.Trigger>
-        </NativeTabs>
-    )
+    return <Tabs tabBar={(props) => <MyTabBar {...props}/>}> 
+        <Tabs.Screen name="index" options={{title:"Home", tabBarIcon: ({color}) => <Ionicons name="home" size={24} color={color}/>}}/>
+        <Tabs.Screen name="profile" options={{title:"Profile", tabBarIcon: ({color})=><Ionicons name="person" size={24} color={color}/> }}/>
+        <Tabs.Screen name="settings" options={{title:"Settings", tabBarIcon: ({color})=><Ionicons name="airplane" size={24} color={color}/> }} />
+    </Tabs>
 }
