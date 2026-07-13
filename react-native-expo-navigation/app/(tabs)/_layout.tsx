eed70aa466e40ee0bd26b31dbf42b59c95ef0a81
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-07-13 05:51:11 +0530

    expo-custom-tab-navigation

--- react-native-expo-navigation/app/(tabs)/_layout.tsx
+++ react-native-expo-navigation/app/(tabs)/_layout.tsx
@@ -1,38 +1,30 @@
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { Tabs } from "expo-router";
 import { TouchableOpacity, View, Text } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons'
 
-function MyTabBar({state, descriptors, navigation}: BottomTabBarProps) {
-    return <View style={{flexDirection:'row', padding:10, gap:4}}>
-        {state.routes.map((route,index) => {
+function MyTabNavigation({state, descriptors, navigation}:BottomTabBarProps) {
+    return <View style={{flexDirection:'row', padding:10}}>
+        { state.routes.map((route, index) => {
+
             const isFocused = state.index === index;
+
             const { options } = descriptors[route.key];
-            const color = isFocused ? "red" : "black";
 
-
-            return <TouchableOpacity 
-                key={route.key}
-                onPress={ () => navigation.navigate(route.name)}
-                style={{flex:1, alignItems:'center',marginBottom:8, gap:2, backgroundColor:'cyan'}}
-            >
-                {options.tabBarIcon?.({
-                    focused: isFocused,
-                    color,
-                    size:24
-                })}
-                <Text style={{color: isFocused ? 'red' : 'black'}}>{options.title}</Text>
-                
-            </TouchableOpacity>
-
+            return <View style={{flex:1, alignItems:'center'}}>
+                <TouchableOpacity key={route.key} onPress={() => navigation.navigate(route.name)} style={{alignItems:'center'}}>
+                    <Ionicons name={route.name==='index' ? 'home' : route.name==='Profile' ? 'person' : 'settings'} size={24} color={isFocused ? 'green' : 'black'}/>
+                    <Text style={{color: isFocused ? 'tomato' : 'black'}}>{options.title}</Text>
+                </TouchableOpacity>
+            </View>
         })}
     </View>
 }
 
 export default function TabLayout() {
-    return <Tabs tabBar={(props) => <MyTabBar {...props}/>}> 
-        <Tabs.Screen name="index" options={{title:"Home", tabBarIcon: ({color}) => <Ionicons name="home" size={24} color={color}/>}}/>
-        <Tabs.Screen name="profile" options={{title:"Profile", tabBarIcon: ({color})=><Ionicons name="person" size={24} color={color}/> }}/>
-        <Tabs.Screen name="settings" options={{title:"Settings", tabBarIcon: ({color})=><Ionicons name="airplane" size={24} color={color}/> }} />
+    return <Tabs tabBar={ (props) => <MyTabNavigation {...props}/>}>
+        <Tabs.Screen name="index" options={{title:"Home"}}/>
+        <Tabs.Screen name="Profile" options={{title:"Profile"}}/>
+        <Tabs.Screen name="Settings" options={{title:"Settings"}}/>
     </Tabs>
 }
--- react-native-expo-navigation/app/(tabs)/settings.tsx
+++ react-native-expo-navigation/app/(tabs)/settings.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const settings = () => {
+const Settings = () => {
   return (
     <View>
-      <Text>settings</Text>
+      <Text>Settings</Text>
     </View>
   )
 }
 
-export default settings
+export default Settings
 
 const styles = StyleSheet.create({})
