65c9650fdc85d6fdab0c4c7bcd3dfe26afe44dd2
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-08-20 07:11:18 +0530

    added-bottom-tabs

--- Projects/Renewly/app/(tabs)/_layout.tsx
+++ Projects/Renewly/app/(tabs)/_layout.tsx
@@ -1,12 +1,60 @@
+import { tabs } from "@/constants/data";
+import { colors, components } from '@/constants/theme';
+import clsx from "clsx";
+import { selectionAsync } from 'expo-haptics';
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const tabBar = components.tabBar;
 
 export default function TabLayout() {
+
+    const insets = useSafeAreaInsets();
+
+    const TabIcon = ({ focused, icon }: TabIconProps) => {
+        return (
+            <View className="tabs-icon">
+                <View className={clsx("tabs-pill", focused && "tabs-active")}>
+                    <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+                </View>
+            </View>
+        );
+    };
+
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="index" options={{title: "Home"}}/>
-            <Tabs.Screen name="insights" options={{title: "Insights"}}/>
-            <Tabs.Screen name="subscriptions" options={{title: "Subscriptions"}}/>
-            <Tabs.Screen name="settings" options={{title: "Settings"}}/>
+        <Tabs screenListeners={{tabPress: () => { selectionAsync() }}} screenOptions={{ headerShown: false , tabBarShowLabel: false,
+            tabBarStyle: {
+                position: 'absolute',
+                bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+                height: tabBar.height,
+                marginHorizontal: tabBar.horizontalInset,
+                borderRadius: tabBar.radius,
+                backgroundColor: colors.primary,
+                borderTopWidth: 0,
+                elevation: 0
+            },
+            tabBarItemStyle: {
+                paddingVertical: tabBar.height / 2 - tabBar.iconFrame/1.6
+            },
+            tabBarIconStyle: {
+                width: tabBar.iconFrame,
+                height: tabBar.iconFrame,
+                alignItems: 'center'
+            }
+         }}>
+            {tabs.map((tab) => (
+                <Tabs.Screen
+                    key={tab.name}
+                    name={tab.name}
+                    options={{
+                        title: tab.title,
+                        tabBarIcon: ({ focused }) => (
+                            <TabIcon focused={focused} icon={tab.icon} />
+                        ),
+                    }}
+                />
+            ))}
         </Tabs>
-    )
+    );
 }
---
+++ Projects/Renewly/type.d.ts
@@ -0,0 +1,11 @@
+import type { ImageSourcePropType } from "react-native";
+
+declare global {
+  interface TabIconProps {
+    icon: ImageSourcePropType;
+    focused: boolean;
+  }
+}
+
+export { };
+
