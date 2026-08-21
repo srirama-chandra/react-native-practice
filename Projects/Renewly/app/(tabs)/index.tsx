ddc64143e18cd996ea5109db9a6402aa74eefc01
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2026-08-21 06:45:46 +0530

    expo-fonts

--- Projects/Renewly/app/(tabs)/index.tsx
+++ Projects/Renewly/app/(tabs)/index.tsx
@@ -6,7 +6,25 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans-light text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans-medium text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans-semibold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-sans-extrabold text-blue-500">
         Welcome to Nativewind!
       </Text>
     </SafeAreaView>
